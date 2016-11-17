9db725b523e629d841370a80f1f9ee1973944584
Dave Weizenegger <Dave Weizenegger>
2016-11-17 15:54:15 -0800

    added hulk image at bottom - scrolls up

--- script/controller.js
+++ script/controller.js
@@ -73,7 +73,7 @@ app.directive("characterList", function(){
                     "charName": "Catwoman",
                     "alias": "Selina Kyle",
                     "studio": "DC Comics",
-                    "alignment": "Neutral",
+                    "alignment": "Villain",
                     "info": "Utilizing a cat mask and suit to steal from the city's rich and corrupt, she's a master at hand-to-hand combat and acrobatics, stealthy as a ninja and an expert at breaking and entering."
                 },
                 {
@@ -146,7 +146,7 @@ app.directive("characterList", function(){
                         "alias": "Eric O'Grady",
                         "studio": "Marvel",
                         "alignment": "Hero",
-                        "info": "O'Grady is a low-level agent of SHIELD who stumbled upon the Ant-Man suit in SHIELD's headquarters. A man of few morals and willing to lie, cheat, steal and manipulate in order to get ahead in life, Eric stole the armor for his own selfish plans, which included using his status as a 'super-hero'to seduce women and humiliate and torment others. He had his own short-lived title before being part of other teams such as joining Avengers: The Initiative as his first team and then joining The Thunderbolts but more recently Secret Avengers, where the character perished heroically while defending a child against the villain known as Father. "
+                        "info": "O'Grady is a low-level agent of SHIELD who stumbled upon the Ant-Man suit in SHIELD's headquarters. A man of few morals and willing to lie, cheat, steal and manipulate in order to get ahead in life, Eric stole the armor for his own selfish plans, which included using his status as a 'super-hero'to seduce women and humiliate and torment others. He had his own short-lived title before being part of other teams such as joining Avengers. "
                     },
                     {
                         "id": "15",
@@ -187,6 +187,118 @@ app.directive("characterList", function(){
                         "studio": "DC Comics",
                         "alignment": "Hero",
                         "info": "Young Barry Allen’s life stopped the minute his mother was murdered. The true killer never found, its mystery obsessed Barry, driving him to become a forensic scientist. Consumed by his work, he spent his life chained to his desk, solving every case that flew across it. But when a freak lightning bolt hits a nearby shelf in his lab, Barry receives super-speed, becoming the Flash. "
+                    },
+                    {
+                        "id": "20",
+                        "charName": "Deadshot",
+                        "alias": "Floyd Lawton",
+                        "studio": "DC Comics",
+                        "alignment": "Villain",
+                        "info": "Part of Gotham City's long line of rogues, Deadshot is obsessed with precision, firmly believing that the target who’s been paid for should be the only one taken down. "
+                    },
+                    {
+                        "id": "22",
+                        "charName": "Aquaman",
+                        "alias": "Arthur Curry",
+                        "studio": "DC Comics",
+                        "alignment": "Hero",
+                        "info": "Cast out from Atlantis as a baby, Arthur Curry grew up on land, thinking himself a normal human. But when he matured, Atlantis returned, claiming him as their rightful king. Caught between a surface world constantly ravaging the sea and Atlanteans looking to lash out in revolt, he’s committed to protecting the entire globe. "
+                    },
+                    {
+                        "id": "23",
+                        "charName": "Bane",
+                        "alias": "Bane",
+                        "studio": "DC Comics",
+                        "alignment": "Villain",
+                        "info": "An orphaned boy imprisoned by tyrants and used as a lab rat for high-risk experiments, Bane broke free from his shackles and grew to become one of the most powerful super-villains in Gotham City. "
+                    },
+                    {
+                        "id": "24",
+                        "charName": "Deathstroke",
+                        "alias": "Slade Wilson",
+                        "studio": "DC Comics",
+                        "alignment": "Villain",
+                        "info": "A military man, Slade Wilson was recruited into the special ops unit Team 7, where he participated in highly dangerous classified missions. Willing to do whatever he could to better himself, Slade underwent an experimental procedure that granted him enhanced speed, strength and intelligence, effectively making him the world's greatest soldier. "
+                    },
+                    {
+                        "id": "25",
+                        "charName": "Joker",
+                        "alias": "Joker",
+                        "studio": "DC Comics",
+                        "alignment": "Villain",
+                        "info": "Not much is known about his past, but his acts during the present are what define the Joker as one of the greatest threats to our heroes and the people they've sworn to protect. He's killed a Robin, crippled Batgirl, and tortured and murdered countless people throughout the DC Universe—all just for a laugh. "
+                    },
+                    {
+                        "id": "26",
+                        "charName": "Lex Luther",
+                        "alias": "Lex Luther",
+                        "studio": "DC Comics",
+                        "alignment": "Villain",
+                        "info": "A self-made man whose immeasurable intellect is always in conflict with his equally immeasurable ego, Lex Luthor is one of the world's most brilliant minds and most famous villains. "
+                    },
+                    {
+                        "id": "27",
+                        "charName": "Mr Freeze",
+                        "alias": "Victor Fries",
+                        "studio": "DC Comics",
+                        "alignment": "Villain",
+                        "info": "An expert in the field of cryogenics, Victor led a relatively normal existence. He worked diligently, but also fell in love with and eventually married a woman named Nora. Sadly, things took an unfortunate turn soon after. Nora became terminally ill, and to stop her degenerative disease from taking her life, Victor put her into suspended animation. His devotion to Nora led to his downfall. "
+                    },
+                    {
+                        "id": "28",
+                        "charName": "Robin",
+                        "alias": "Dick Grayson",
+                        "studio": "DC Comics",
+                        "alignment": "Hero",
+                        "info": "Acrobat Dick Grayson was the youngest member of his parents’ death-defying circus act. But after their cold-blooded murder, Dick vowed revenge. Seeing a genuine piece of himself in the boy, Batman took him in—and gave him a better outlet for his anger.  "
+                    },
+                    {
+                        "id": "29",
+                        "charName": "Scarecrow",
+                        "alias": "Jonathon Crane",
+                        "studio": "DC Comics",
+                        "alignment": "Villain",
+                        "info": "As a young child, Jonathan Crane was subjected to sick and twisted experiments on fear conducted by his own father. Emotionally scarred and mentally unstable from the ordeal, Crane spent his childhood living in fear until he made an important realization—if he could control fear, it would never again control him."
+                    },
+                    {
+                        "id": "30",
+                        "charName": "Two-face",
+                        "alias": "Harvey Dent",
+                        "studio": "DC Comics",
+                        "alignment": "Villain",
+                        "info": "A prominent and well-educated District Attorney, Harvey Dent fought valiantly to uphold the law and make the city a better place. In any other city, it would have worked. Not in Gotham—a place where greed and corruption stretches from the depths of Crime Alley to the shimmering heights of the mayor’s office. Dent’s spiral into Gotham’s madness came swiftly after a criminal threw acid into his face, leaving half of it horribly scarred. Seeking refuge in the underbelly of Gotham, the name Harvey Dent and all the good it stood for was lost…and Two-Face rose from the darkness."
+                    },
+                    {
+                        "id": "31",
+                        "charName": "Captain Marvel",
+                        "alias": "Carol Danvers",
+                        "studio": "Marvel",
+                        "alignment": "Hero",
+                        "info": "Carol Danvers entered the Air Force upon graduating from high school to pursue her love of aircrafts and her dreams of flying. Carol was a top student at the Air Force Academy and enjoyed a rapid career advancement rising to the rank of Major. Because of her stellar performance, superb combat skills and natural intellect, Carol was recruited into the intelligence field."
+                    },
+                    {
+                        "id": "32",
+                        "charName": "Captain America",
+                        "alias": "Steve Rogers",
+                        "studio": "Marvel",
+                        "alignment": "Hero",
+                        "info": "Vowing to serve his country any way he could, young Steve Rogers took the super soldier serum to become America's one-man army. Fighting for the red, white and blue for over 60 years, Captain America is the living, breathing symbol of freedom and liberty."
+                    },
+                    {
+                        "id": "33",
+                        "charName": "Dr Strange",
+                        "alias": "Stephan Strange",
+                        "studio": "Marvel",
+                        "alignment": "Hero",
+                        "info": "Doctor Strange is one of the most powerful sorcerers in existence. Like most sorcerers, he draws his power from three primary sources: the invocation of powerful mystic entities or objects, the manipulation of the universe's ambient magical energy, and his own psychic resources."
+                    },
+                    {
+                        "id": "34",
+                        "charName": "Green Goblin",
+                        "alias": "Norman Osborn",
+                        "studio": "Marvel",
+                        "alignment": "Villain",
+                        "info": "The father of Peter Parker's best friend, Harry, businessman Norman Osborn moonlights as the unstable Green Goblin and has caused Spider-Man more tragedy than any other foe!"
                     }
             ];
         },
